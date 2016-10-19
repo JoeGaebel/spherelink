5d38d4b18a6678cd1536ba33c1059221d8590c5b
Joe Gaebel <joe.gaebel@hotmail.com>
2016-10-19 19:16:20 -0400

    Update spec helpers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,9 @@
 require 'rubygems'
 
 ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path(File.join(File.dirname(__FILE__), '..', 'config', 'environment'))
+require File.expand_path('../../config/environment', __FILE__)
+abort('The Rails environment is running in production mode!') if Rails.env.production?
+ActiveRecord::Migration.maintain_test_schema!
 
 unless defined?(TESTS_ARE_LOADED)
   # silence_warnings { BCrypt::Engine::DEFAULT_COST = BCrypt::Engine::MIN_COST }
@@ -9,11 +11,15 @@ unless defined?(TESTS_ARE_LOADED)
   require 'rspec/rails'
   require 'rspec/expectations'
   require 'shoulda/matchers'
+  require 'capybara/rspec'
 end
 
 
 RSpec.configure do |config|
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
+  config.filter_rails_from_backtrace!
 
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
