a54805c9c2c5e69c0c72efb1bec4240b50f3af85
JoeGaebel <joe@Joes-MacBook-Pro.local>
2017-04-28 01:21:51 -0400

    Clean up seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,3 @@
-USERS_COUNT = 15
-POSTS_COUNT = 5
-
 joe = User.create!({
   name:  'Joe',
   email: 'joe@joegaebel.com',
@@ -10,32 +7,6 @@ joe = User.create!({
   activated: true,
   activated_at: Time.zone.now
 })
-
-# USERS_COUNT.times do |n|
-#   User.create!({
-#     name: Faker::Name.name,
-#     email: "example-#{n+1}@example.com",
-#     password: 'password',
-#     password_confirmation: 'password',
-#     activated: true,
-#     activated_at: Time.zone.now
-#   })
-# end
-
-# users = User.order(:created_at).take(USERS_COUNT/3)
-# POSTS_COUNT.times do
-#   users.each do |user|
-#     content = Faker::Hipster.sentence(4, false, 4)
-#     user.microposts.create!(content: content)
-#   end
-# end
-#
-# users = User.all
-# user  = users.first
-# following = users[2..USERS_COUNT]
-# followers = users[3..USERS_COUNT]
-# following.each { |followed| user.follow(followed) }
-# followers.each { |follower| follower.follow(user) }
 
 puts "Creating memory stuff"
 
