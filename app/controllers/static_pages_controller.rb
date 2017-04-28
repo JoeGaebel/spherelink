d5d9819eca5087b8592ba398cabc341055ac1688
JoeGaebel <joe@Joes-MacBook-Pro.local>
2017-04-28 00:29:39 -0400

    Conditionally apply the background image

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,6 +4,7 @@
       @micropost  = current_user.microposts.build
       @feed_items = current_user.feed.paginate(page: params[:page])
     else
+      @body_class = 'demo'
       @memory_json = Memory.second.to_builder.target!
     end
   end
