2a5fe4bfea9338b0f116ef4676ed76a387158fe3
Joe Gaebel <joe@mavenlink.com>
2017-06-04 14:39:21 -0400

    Abstract out the EditMemoryApplication from the MemoryApplication

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,8 +19,8 @@
 //= require uevent/uevent
 //= require dot/doT
 //= require photo-sphere-viewer/dist/photo-sphere-viewer
-//= require memory_application
 //= require soundmanager2/script/soundmanager2-jsmin
 //= require nested_form_fields
 //= require tipsy
+//= require_directory ./applications/
 //= require_tree .
