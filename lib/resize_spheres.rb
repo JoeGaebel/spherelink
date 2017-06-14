60ff808267a49c266e21e23671b0d56470437d2e
Joe Gaebel <joe@mavenlink.com>
2017-06-14 01:44:54 -0400

    Remove dimension check due to Fog storage

--- lib/resize_spheres.rb
+++ lib/resize_spheres.rb
@@ -1,22 +1,7 @@
 class ResizeSpheres
   def self.run
-    history = []
     Sphere.all.each do |sphere|
-      dimensions = self.dimensions(sphere.panorama.file.file)
-      if dimensions[0] > SphereUploader::MAX_WIDTH.to_s
-        sphere.panorama.recreate_versions!
-        new_dimensions = self.dimensions(sphere.panorama.file.file)
-        history << {
-          old_dimensions: dimensions,
-          new_dimensions: new_dimensions
-        }
-      end
+      sphere.panorama.recreate_versions!
     end
-
-    history
-  end
-
-  def self.dimensions(file_path)
-    `identify -format "%wx%h" #{file_path}`.split(/x/)
   end
 end
