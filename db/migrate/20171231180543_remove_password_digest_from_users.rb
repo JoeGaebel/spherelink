21b9836c1fed5b3eaf36231a75df2ab40993baff
Joe Gaebel <joe@mavenlink.com>
2018-01-03 14:43:51 -0500

    fix the autofocus

---
+++ db/migrate/20171231180543_remove_password_digest_from_users.rb
@@ -0,0 +1,11 @@
+class RemovePasswordDigestFromUsers < ActiveRecord::Migration[5.0]
+  def change
+    remove_column :users, :password_digest
+    remove_column :users, :remember_digest
+    remove_column :users, :activation_digest
+    remove_column :users, :activated
+    remove_column :users, :activated_at
+    remove_column :users, :reset_digest
+    remove_column :users, :reset_sent_at
+  end
+end
