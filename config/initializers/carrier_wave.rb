2d8a0dbecfd06bdde05404e65f56bcf45ce87c3e
Joe Gaebel <joe.gaebel@hotmail.com>
2018-01-29 19:46:09 -0500

    Try a different caching line

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -7,7 +7,7 @@ if Rails.env.production?
       :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
     config.fog_directory     =  ENV['S3_BUCKET_NAME']
-    config.fog_attributes = { cache_control: "public, max-age=#{30.days.to_i}" }
+    config.fog_attributes = { 'Cache-Control'=>'max-age=315576000', 'Expires' => 1.week.from_now.httpdate }
   end
 end
 
